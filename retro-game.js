838d6ad0e75e117d0354cf5554d615a64effb2a8
Dirk-FHDW <dirk.soltenborn@edu.fhdw.de>
2018-07-18 11:19:13 +0200

    Lives and Score

--- retro-game.js
+++ retro-game.js
@@ -1,39 +1,63 @@
+
+
 //get Canvas
 var spiel = document.getElementById('game');
 var context = spiel.getContext('2d');
 
-var plane=[]
-var vehicle=[]
+//localStorage.setItem('highscore',0)
+//console.log(localStorage.getItem('highscore'))      //personal highscore!?
+
+
+
+//frog json
+var frog = {
+    x: spiel.width / 2 - 15,
+    y: spiel.height - 55,
+    picture: 0
+};
+
+var plane = []
+var vehicle = []
 var gameStatus = true
+var lives = 4
+try{
+    highscore = localStorage.getItem('highscore')
+}catch(exception){
+    highscore = 0
+}
+
+
+
+var score = 0
 //set images
 sprites = new Image();
 sprites.src = 'assets/frogger.png';
 sprites.onload = function () { //after images are load
 
-deadSprite = new Image()
-deadSprite.src = 'assets/dead_frog.png'
+    deadSprite = new Image()
+    deadSprite.src = 'assets/dead_frog.png'
 
 
 
 
     //Create Cars
     for (i = 0; i < 3; i++) {
-        vehicle[i] = new gameObject(50 * i,460, "pink car",  2, "right")
+        vehicle[i] = new gameObject(50 * i, 460, "pink car", 4, "right")
 
     }
     for (i = 3; i < 5; i++) {
-        vehicle[i] = new gameObject(50 * i,430, "red car", 2.5, "left")
+        vehicle[i] = new gameObject(50 * i, 430, "red car", 6, "left")
     }
 
     for (i = 5; i < 7; i++) {
-        vehicle[i] = new gameObject(50 * i,400, "white LKW", 2.3, "right", 1.2)
+        vehicle[i] = new gameObject(50 * i, 400, "white LKW", 4, "right", 1.2)
 
     }
     for (x = 0; x < 2; x++) {
-        plane[x] = new gameObject(120 * x,250, "wood line", 2, "left", 4)
+        plane[x] = new gameObject(120 * x, 250, "wood line", 5, "left", 4)
     }
     for (x = 2; x < 4; x++) {
-        plane[x] = new gameObject(120 * (x - 2),220, "wood line", 2, "right", 4)
+        plane[x] = new gameObject(120 * (x - 2), 220, "wood line", 5, "right", 4)
     }
 
     loop = setInterval(game_loop, 16); //loop //85 mileseconds = ~  30 FPS (motion human able to see)
@@ -44,30 +68,29 @@ deadSprite.src = 'assets/dead_frog.png'
 
 //-----------------------loop event---------------------------
 
- var game_loop = function () {
-console.log("loop")
+var game_loop = function () {
+    context.beginPath()
 
     drawBackground();
-    
+
 
     for (x = 0; x < Object.keys(plane).length; x++) {
-       
+
         plane[x].move();
-        if(plane[x].checkOn()){
-            
-            plane[x].direction == "left" ? frog.x = frog.x + plane[x].speed :frog.x = frog.x - plane[x].speed
+        if (plane[x].checkOn()) {
+
+            plane[x].direction == "left" ? frog.x = frog.x + plane[x].speed : frog.x = frog.x - plane[x].speed
         }
-        
-        
+
+
     }
 
 
     for (i = 0; i < Object.keys(vehicle).length; i++) {
         vehicle[i].move(); //loop through all cars
-       if( vehicle[i].checkOn()){
-        gameStatus = false
-        clearInterval(loop)
-       } 
+        if (vehicle[i].checkOn()) {
+            dead();
+        }
     }
 
 
@@ -81,16 +104,9 @@ console.log("loop")
             } else {
                 bool = false
             }
-
-
         }
-        if (bool) {
-            console.log("nowater")
-
-        } else {
-            console.log("water")
-            gameStatus = false
-            clearInterval(loop)
+        if (!bool) {
+            dead()
         }
 
 
@@ -112,37 +128,49 @@ var drawBackground = function () {
     context.fillStyle = '#000000'; //new color after first 
     context.fillRect(0, 284, 399, 283); //fill style
     // image,x old, y old, width old, height old, x new, y new, width new, height new
-    context.drawImage(sprites, 0, 0, 399, 113, 0, 0, 399, 113);
-    context.drawImage(sprites, 0, 119, 399, 34, 0, 283, 399, 34);
-    context.drawImage(sprites, 0, 119, 399, 34, 0, 495, 399, 34);
+    context.drawImage(sprites, 0, 0, 399, 113, 0, 0, 399, 113); // lila on top of page
+    context.drawImage(sprites, 0, 119, 399, 34, 0, 283, 399, 34); //lila between water and street
+    context.drawImage(sprites, 0, 119, 399, 34, 0, 495, 399, 34); //lila at the beginning
 
-    context.arc(112,90,10,0,2*Math.PI);
+    context.arc(112, 90, 10, 0, 2 * Math.PI); //water lily
     context.stroke();
-    context.fillStyle='green';
+    context.fillStyle = 'green';
     context.fill()
+
+    context.font = 'bold 12pt arial';
+    context.fillText('Score: ', 100, 550);
+    context.fillText('Highscore: ', 240, 550);
+    context.fillText(score, 155 , 550);
+    context.fillText(highscore, 330 , 550);
+    console.log(lives)
+    for (i = 0; i < lives; i++) {
+        context.drawImage(sprites, 13, 334, 17, 23, 5 + i * 20, 538, 11, 15);
+    }
+
+
 };
 
 var drawFrog = function () {
-if(gameStatus != false){
+    if (gameStatus != false) {
 
 
-    switch (frog.picture) {
-        case 0:
-            context.drawImage(sprites, 12, 369, 23, 20, frog.x, frog.y, 23, 20); // draw frog up
-            break;
-        case 1:
-            context.drawImage(sprites, 80, 369, 23, 20, frog.x, frog.y, 23, 20); // draw frog down
-            break;
-        case 2:
-            context.drawImage(sprites, 75, 337, 23, 20, frog.x, frog.y, 23, 20); // draw frog left
-            break;
-        case 3:
-            context.drawImage(sprites, 12, 335, 23, 22, frog.x, frog.y, 23, 22); // draw frog right
-            break;
+        switch (frog.picture) {
+            case 0:
+                context.drawImage(sprites, 12, 369, 23, 20, frog.x, frog.y, 23, 20); // draw frog up
+                break;
+            case 1:
+                context.drawImage(sprites, 80, 369, 23, 20, frog.x, frog.y, 23, 20); // draw frog down
+                break;
+            case 2:
+                context.drawImage(sprites, 75, 337, 23, 20, frog.x, frog.y, 23, 20); // draw frog left
+                break;
+            case 3:
+                context.drawImage(sprites, 12, 335, 23, 22, frog.x, frog.y, 23, 22); // draw frog right
+                break;
+        }
+    } else {
+        context.drawImage(deadSprite, 0, 0, 40, 30, frog.x, frog.y - 10, 40, 30);
     }
-}else{
-    context.drawImage(deadSprite, 0, 0, 40, 30, frog.x, frog.y-10, 40, 30); 
-}
 }
 
 var checkWater = function () {
@@ -155,32 +183,36 @@ var checkWater = function () {
 //---------------keydown event------------------------
 
 document.body.onkeydown = function (event) {
-if(gameStatus!= false){
-    if (event.keyCode == 38 && frog.y - 100 > 0) {
-        frog.picture = 0;
-        frog.y = frog.y - 35;
-        
-
-    } else if (event.keyCode == 40 && frog.y < spiel.height - 80) {
-        frog.picture = 1;
-        frog.y = frog.y + 35;
-    } else if (event.keyCode == 37 && frog.x > 40) {
-        frog.picture = 2;
-        frog.x = frog.x - 42;
-    } else if (event.keyCode == 39 && frog.x < spiel.width - 50) {
-        frog.picture = 3;
-        frog.x = frog.x + 42;
-
+    console.log(frog.y)
+    if (gameStatus != false) { //if crashed then press key to reload
+        if (event.keyCode == 38 && frog.y - 100 > 0) {
+            frog.picture = 0; //frog up
+            frog.y = frog.y - 35;
+     
+            if((505 - frog.y) / 35 *100 > score){
+            
+                score = (505 - frog.y) / 35 *100 
+               
+            } 
+        } else if (event.keyCode == 40 && frog.y < spiel.height - 80) {
+            frog.picture = 1; //frog down
+            frog.y = frog.y + 35;
+        } else if (event.keyCode == 37 && frog.x > 40) {
+            frog.picture = 2; //frog left
+            frog.x = frog.x - 42;
+        } else if (event.keyCode == 39 && frog.x < spiel.width - 50) {
+            frog.picture = 3; //frog right
+            frog.x = frog.x + 42;
+        }
+    } else {
+        gameStatus = true
+        frog = frog = {
+            x: spiel.width / 2 - 15,
+            y: spiel.height - 55,
+            picture: 0
+        };
+        loop = setInterval(game_loop, 16)
     }
-}else{
-    gameStatus = true
-    frog= frog = {
-        x: spiel.width / 2 - 15,
-        y: spiel.height - 55,
-        picture: 0
-    };
-    loop = setInterval(game_loop,16)
-}
 
 };
 
@@ -191,7 +223,7 @@ if(gameStatus!= false){
 
 
 
-var gameObject = function (x, y,vehicle, speed, direc, length) { //parameter x y bzw row and speed have to be given
+var gameObject = function (x, y, vehicle, speed, direc, length) { //parameter x y bzw row and speed have to be given
     this.x = x
     this.y = y
     this.vehicle = vehicle
@@ -204,6 +236,7 @@ var gameObject = function (x, y,vehicle, speed, direc, length) { //parameter x y
 
         switch (this.direction) {
             case "left":
+
                 this.x <= spiel.width + this.lenght * 20 ? this.x = (this.x + this.speed) : this.x = -this.lenght * 20; //move right
                 break;
             case "right":
@@ -211,10 +244,9 @@ var gameObject = function (x, y,vehicle, speed, direc, length) { //parameter x y
         }
 
 
-        switch (this.vehicle) {
+        switch (this.vehicle) { //draw objects
             case "pink car":
-            
-                context.drawImage(sprites, 8, 265, 30, 22, this.x, this.y , 30, 22); //pink car
+                context.drawImage(sprites, 8, 265, 30, 22, this.x, this.y, 30, 22); //pink car
                 break;
             case "red car":
                 context.drawImage(sprites, 44, 265, 30, 22, this.x, this.y, 30, 22); //car red wheel
@@ -223,33 +255,39 @@ var gameObject = function (x, y,vehicle, speed, direc, length) { //parameter x y
                 context.drawImage(sprites, 100, 300, 60, 22, this.x, this.y, 60, 22); //LKW
                 break;
             case "wood line":
-                context.drawImage(sprites, 10, 225, 85, 28, this.x, this.y, 85, 28);
+                context.drawImage(sprites, 10, 225, 85, 28, this.x, this.y, 85, 28); //wood
                 break;
-           
+
         }
 
     }
     this.checkOn = function () {
-        
-        //if in hitbox of frog alert crash
-      
-        if(frog.x+10 >= this.x && frog.x-10 <= this.x + this.lenght * 20 && frog.y <= this.y +10 && frog.y >= this.y -10 ){
-            console.log("he")
+
+        if (frog.x + 10 >= this.x && frog.x - 10 <= this.x + this.lenght * 20 && frog.y <= this.y + 10 && frog.y >= this.y - 10) {
+            //true if frog in hitbox
             return true
-             
-        }else{
-            return false
+
         }
 
 
     }
 }
 
-
-
-
-var frog = {
-    x: spiel.width / 2 - 15,
-    y: spiel.height - 55,
-    picture: 0
-};
+var dead = function () {
+    gameStatus = false
+    lives = lives - 1
+    clearInterval(loop)
+    if (lives == 0) {
+        
+        alert("lose game")
+        if(highscore<score){
+           
+            localStorage.setItem('highscore',score)
+            highscore=score
+            
+        }
+        score =0
+        lives = 4
+     
+    }
+}
