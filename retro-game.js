3d9417db438d21449ba683837a26b00979174c0b
Yannik Kasper <ykasper84@gmail.de>
2018-07-21 23:13:56 +0200

    dead on site, more turtles, win animation, code structure

--- retro-game.js
+++ retro-game.js
@@ -9,6 +9,7 @@ theme.setAttribute('loop', 'true');
 var jumpSound = document.createElement('audio');
 jumpSound.setAttribute('src', 'assets/jump3.mp3');
 areSoundsActive = true
+
 //theme.play();
 
 //frog json
@@ -18,13 +19,14 @@ var frog = {
     picture: "up"
 };
 
-var plane = []
+var waterObj = []
 var vehicle = []
-var turtle = []
-var crocodile =[]
+
 var isPlayerAlive = true
 var lives = 4
 var music = true
+var win = [false, false, false, false, false]
+var keyPressed = false
 
 try {
 
@@ -46,49 +48,8 @@ sprites.onload = function () { //after images are load
 
     deadSprite = new Image()
     deadSprite.src = 'assets/dead_frog.png'
-
-
-
-
+    createObjects();
     //Create Cars
-    for (i = 0; i < 3; i++) {
-        vehicle[i] = new gameObject(50 * i, 465, "pink car", 4, "right")
-
-    }
-    for (i = 3; i < 5; i++) {
-        vehicle[i] = new gameObject(50 * i, 432, "red car", 6, "left")
-    }
-
-    for (i = 5; i < 7; i++) {
-        vehicle[i] = new gameObject(50 * i, 396, "white LKW", 4, "right", 1.2)
-
-    }
-    for (i = 7; i < 11; i++) {
-        vehicle[i] = new gameObject(40 * (i - 4), 360, "bagger", 4, "left")
-
-    }
-    for (i = 11; i < 13; i++) {
-        vehicle[i] = new gameObject(40 * (i - 6), 325, "blue car", 4, "right")
-
-    }
-    for (x = 0; x < 2; x++) {
-        plane[x] = new gameObject(120 * x, 250, "wood line", 2, "left", 2.5)
-    }
-    for (x = 2; x < 4; x++) {
-        plane[x] = new gameObject(120 * x, 215, "wood line", 3, "right", 2.5)
-    }
-
-    for(x=0;x<3;x++){
-        turtle[x] = new gameObject(50 * x,180,"turtle1",2,"right")
-    }
-    for(x=0;x<2;x++){
-        crocodile[x]= new gameObject(100*x,145,"crocodile1",1,"left",2)
-        
-    }
-    for (x = 4; x < 6; x++) {
-        plane[x] = new gameObject(120 * (x - 2), 110, "wood line", 1, "right", 2.5)
-    }
-
     loop = setInterval(game_loop, 16); //loop //85 mileseconds = ~  30 FPS (motion human able to see)
 
 };
@@ -98,90 +59,42 @@ sprites.onload = function () { //after images are load
 //-----------------------loop event---------------------------
 var counter = 0
 var fx = 1
+
+
 var game_loop = function () {
-    context.beginPath()
-    
+
+
     drawBackground();
-
-
-        for (x = 0; x < Object.keys(plane).length; x++) {
-
-        plane[x].move();
-        if (plane[x].checkOn()) {
-
-            plane[x].direction == "left" ? frog.x = frog.x + plane[x].speed : frog.x = frog.x - plane[x].speed
-        }
-
-
-    }
-  
-
-    counter = (counter + 2 * fx )% 602
-  
-    for (x = 0; x < Object.keys(turtle).length; x++) {
-        turtle[x].move()
-        counter == 600 || counter == 0 ? fx = fx * -1 : null
-        if(counter % 50 == 0) {
-            turtle[x].vehicle = "turtle" + (counter / 50)   
-        }
-    }
-
-
-
-    for (x = 0; x < Object.keys(crocodile).length; x++) {
-        crocodile[x].move() 
-    }
-
-    for (i = 0; i < Object.keys(vehicle).length; i++) {
-        vehicle[i].move(); //loop through all cars
-        if (vehicle[i].checkOn()) {
-            dead();
-        }
-    }
-
-
-
-    if (checkWater() && !frog.picture.includes("way")) {
-        var isPlayerOnSafeObject = false
-        for (i = 0; i < Object.keys(plane).length; i++) {
-            //every
-            if (plane[i].checkOn() && !isPlayerOnSafeObject) {
-                isPlayerOnSafeObject = true
-                break;
-            } else {
-                isPlayerOnSafeObject = false
+    moveObjects();
+    if (!frog.picture.includes("way")) {
+        if (!checkWin()) {
+            if (checkWater()) {
+                if (!checkOnSaveObject() || frog.x > spiel.width - 20 || frog.x < -10) {
+                    dead();
+                }
             }
-        }
-        if (!isPlayerOnSafeObject) {
-            dead()
-        }
 
-
+        }
     }
 
-    if ((505 - frog.y) / 35 * 100 > score && !frog.picture.includes("way")) {
-        score = (505 - frog.y) / 35 * 100 //new score  
-    }
-
-
-
-
+    checkScore();
     drawFrog(); //have to be on the end because on top of all images
 }
 
 //----------------------------draw background------------ 
 
 var drawBackground = function () {
+    context.beginPath()
     context.fillStyle = '#191970'; //color
-    context.fillRect(0, 0, 399, 284); //fill syle
+    context.fillRect(0, 0, 400, 284); //fill syle
     context.fillStyle = '#000000'; //new color after first 
-    context.fillRect(0, 284, 399, 283); //fill style
+    context.fillRect(0, 284, 400, 283); //fill style
     // image,x old, y old, width old, height old, x new, y new, width new, height new
     context.drawImage(sprites, 0, 0, 399, 113, 0, 0, 399, 113); // yelow on top of page
     context.drawImage(sprites, 0, 119, 399, 34, 0, 283, 400, 38); //lila between water and street
     context.drawImage(sprites, 0, 119, 399, 34, 0, 490, 400, 38); //lila at the beginnng
 
-    
+
 
     for (i = 0; i < 5; i++) {
         context.beginPath()
@@ -191,8 +104,8 @@ var drawBackground = function () {
         context.fill()
     }
 
-    drawLine(5, 460)        //recursive function just because I can
-  
+    drawLine(5, 460) //recursive function just because I can
+
 
 
     context.font = 'bold 12pt arial';
@@ -204,12 +117,67 @@ var drawBackground = function () {
         context.drawImage(sprites, 13, 334, 17, 23, 5 + i * 20, 538, 11, 15);
     }
 
+    for (i = 0; i < 5; i++) {
+        if (win[i] == true) {
+            context.drawImage(sprites, 12, 369, 23, 20, 15 + (i * 85), 82, 23, 20)
+        }
+    }
+
 
 };
 
-var drawLine = function (x, y) {        
+var checkOnSaveObject = function () {
 
-    context.beginPath()     //recursive function just because I can
+    for (i = 0; i < Object.keys(waterObj).length; i++) {
+        //every
+
+        if (waterObj[i].checkOn() && waterObj[i].vehicle != "turtle9") {
+
+            return true
+
+        }
+    }
+
+    return false
+
+
+}
+
+var moveObjects = function () {
+    counter = (counter + 2 * fx) % 602
+    for (x = 0; x < Object.keys(waterObj).length; x++) {
+        waterObj[x].move();
+
+        if (waterObj[x].checkOn()) {
+            waterObj[x].direction == "right" ? frog.x = frog.x + waterObj[x].speed : frog.x = frog.x - waterObj[x].speed
+
+        }
+
+        counter == 450 || counter == 0 ? fx = fx * -1 : null
+        if (counter % 50 == 0 && waterObj[x].vehicle.includes("turtle")) {
+            waterObj[x].vehicle = "turtle" + (counter / 50)
+
+        }
+
+    }
+
+    for (i = 0; i < Object.keys(vehicle).length; i++) {
+        vehicle[i].move(); //loop through all cars
+        if (vehicle[i].checkOn()) {
+            dead();
+        }
+    }
+}
+
+var checkScore = function () {
+    if ((505 - frog.y) / 35 * 100 > score && !frog.picture.includes("way")) {
+        score = (505 - frog.y) / 35 * 100 //new score  
+    }
+}
+
+var drawLine = function (x, y) {
+
+    context.beginPath() //recursive function just because I can
     context.moveTo(x, y)
     context.lineTo(x + 30, y)
     context.strokeStyle = "white"
@@ -228,9 +196,9 @@ var drawLine = function (x, y) {
 var drawFrog = function () {
     if (isPlayerAlive != false) {
 
-        
+
         switch (frog.picture) {
-            
+
             case "up":
                 context.drawImage(sprites, 12, 369, 23, 20, frog.x, frog.y, 23, 20); // draw frog up
                 break;
@@ -262,58 +230,65 @@ var drawFrog = function () {
 }
 
 var checkWater = function () {
+
     if (frog.y <= spiel.height / 2) {
         return true
     }
+    return false
 }
 
 
 //---------------keydown event------------------------
 
 document.body.onkeydown = async function (event) { //async because we want to await
+    if (keyPressed == false) {
+        if (isPlayerAlive != false) { //if crashed then press key to reload
+            playSound()
+            keyPressed = true
+            if (event.keyCode == 38 && frog.y - 100 > 0) { //move and change picture while jumping 100 ms
 
-    if (isPlayerAlive != false) { //if crashed then press key to reload
-        playSound()
-        if (event.keyCode == 38 && frog.y - 100 > 0) { //move and change picture while jumping 100 ms
-            
-            frog.picture = "upway"; //frog up
-            frog.y = frog.y - 25;
-            await sleep(100)
-            frog.picture = "up"; //frog left
-            frog.y = frog.y - 10;
+                frog.picture = "upway"; //frog up
+                frog.y = frog.y - 25;
+                await sleep(100)
+                frog.picture = "up"; //frog left
+                frog.y = frog.y - 10;
 
-        } else if (event.keyCode == 40 && frog.y < spiel.height - 80) {
-            frog.picture = "downway"; //frog down
-            frog.y = frog.y + 20;
-            await sleep(100)
-            frog.picture = "down"; //frog left
-            frog.y = frog.y + 15;
+            } else if (event.keyCode == 40 && frog.y < spiel.height - 80) {
+                frog.picture = "downway"; //frog down
+                frog.y = frog.y + 20;
+                await sleep(100)
+                frog.picture = "down"; //frog left
+                frog.y = frog.y + 15;
 
-        } else if (event.keyCode == 37 && frog.x > 40) {
-            frog.picture = "leftway"; //frog left
-            frog.x = frog.x - 21;
-            await sleep(100)
-            frog.picture = "left"; //frog left
-            frog.x = frog.x - 21;
+            } else if (event.keyCode == 37 && frog.x > 40) {
+                frog.picture = "leftway"; //frog left
+                frog.x = frog.x - 21;
+                await sleep(100)
+                frog.picture = "left"; //frog left
+                frog.x = frog.x - 21;
 
-        } else if (event.keyCode == 39 && frog.x < spiel.width - 50) {
-            frog.picture = "rightway"; //frog right
-            frog.x = frog.x + 21;
-            await sleep(100)
-            frog.picture = "right"; //frog left
-            frog.x = frog.x + 21;
+            } else if (event.keyCode == 39 && frog.x < spiel.width - 50) {
+                frog.picture = "rightway"; //frog right
+                frog.x = frog.x + 21;
+                await sleep(100)
+                frog.picture = "right"; //frog left
+                frog.x = frog.x + 21;
+            }
+        } else {
+            isPlayerAlive = true
+            frog = frog = {
+                x: spiel.width / 2 - 15,
+                y: spiel.height - 55,
+                picture: "up"
+            };
+            loop = setInterval(game_loop, 16)
         }
-    } else {
-        isPlayerAlive = true
-        frog = frog = {
-            x: spiel.width / 2 - 15,
-            y: spiel.height - 55,
-            picture: "up"
-        };
-        loop = setInterval(game_loop, 16)
     }
-
 };
+
+document.body.onkeyup = function () {
+    keyPressed = false
+}
 
 //sleep function returns and promise after x miliseconds so a async function can wait while executing
 var sleep = function (ms) {
@@ -334,12 +309,11 @@ var gameObject = function (x, y, vehicle, speed, direc, length) { //parameter x 
     this.move = function () {
 
         switch (this.direction) {
-            case "left":
-
-                this.x <= spiel.width + this.lenght * 20 ? this.x = (this.x + this.speed) : this.x = -this.lenght * 20; //move right
-                break;
             case "right":
-                this.x >= (-20 * this.lenght) ? this.x = (this.x - this.speed) : this.x = spiel.width + this.lenght * 20; //move left
+                this.x <= spiel.width + this.lenght * 40 ? this.x = (this.x + this.speed) : this.x = -this.lenght * 40; //move right
+                break;
+            case "left":
+                this.x >= (-40 * this.lenght) ? this.x = (this.x - this.speed) : this.x = spiel.width + this.lenght * 40; //move left
         }
 
 
@@ -366,26 +340,26 @@ var gameObject = function (x, y, vehicle, speed, direc, length) { //parameter x 
                 context.drawImage(sprites, 155, 370, 90, 25, this.x, this.y, 90, 25); //crocodile
                 break;
             case "crocodile2":
-                context.drawImage(sprites, 155, 330, 90, 35, this.x,this.y, 90, 35); //crocodile2
+                context.drawImage(sprites, 155, 330, 90, 35, this.x, this.y, 90, 35); //crocodile2
                 break;
             case "turtle0":
             case "turtle1":
-            case "turtle2":         //fall through for longer tutrle 0
+            case "turtle2": //fall through for longer tutrle 0
             case "turtle3":
             case "turtle4":
-                context.drawImage(sprites, 16, 405, 26, 22, this.x,this.y, 26, 22); //turtle 1
+                context.drawImage(sprites, 16, 405, 26, 22, this.x, this.y, 26, 22); //turtle 1
                 break;
             case "turtle5":
-                context.drawImage(sprites, 55, 405, 26, 24, this.x,this.y, 26, 24); //turtle2
+                context.drawImage(sprites, 55, 405, 26, 24, this.x, this.y, 26, 24); //turtle2
                 break;
             case "turtle6":
-                context.drawImage(sprites, 92, 405, 30, 24, this.x,this.y, 30, 24); //turtle3
+                context.drawImage(sprites, 92, 405, 30, 24, this.x, this.y, 30, 24); //turtle3
                 break;
             case "turtle7":
-                context.drawImage(sprites, 134, 405, 30, 24, this.x,this.y, 30, 24); //turtle4
+                context.drawImage(sprites, 134, 405, 30, 24, this.x, this.y, 30, 24); //turtle4
                 break;
             case "turtle8":
-                context.drawImage(sprites, 180, 405, 20, 20, this.x,this.y, 20, 20); //turtle5
+                context.drawImage(sprites, 180, 405, 20, 20, this.x, this.y, 20, 20); //turtle5
                 break;
             case "turtle9":
                 break;
@@ -407,13 +381,33 @@ var gameObject = function (x, y, vehicle, speed, direc, length) { //parameter x 
 
     }
 }
+var checkWin = function () {
+
+    for (i = 0; i < 5; i++) {
+        if (frog.y < 100 && frog.x > 5 + (85 * i) && frog.x < 30 + (85 * i)) {
+            win[i] = true
+            //score = score +200
+            frog.x = spiel.width / 2 - 15
+            frog.y = spiel.height - 55
+            return true
+        }
+    }
+
+    if (win.every(function (elemet) {
+            return elemet == true
+        })) {
+        alert("won game")
+    }
+    return false
+
+}
 
 var dead = function () {
     isPlayerAlive = false
     lives = lives - 1
     clearInterval(loop)
     if (lives == 0) {
-
+        win = [false, false, false, false, false]
         alert("lose game")
         if (highscore < score) {
 
@@ -427,13 +421,13 @@ var dead = function () {
     }
 }
 
-var musicOff = function(){
+var musicOff = function () {
     var musicButton = document.getElementById("musicBtn")
-    if(music==true){
+    if (music == true) {
         theme.pause()
         musicButton.innerHTML = "Music an"
         music = false
-    }else{
+    } else {
         theme.play()
         musicButton.innerHTML = "Music aus"
         music = true
@@ -443,23 +437,71 @@ var musicOff = function(){
 
 }
 
-var playSound = function(){
-    
-    if(areSoundsActive == true){
-     
+var playSound = function () {
+
+    if (areSoundsActive == true) {
+
         jumpSound.load()
         jumpSound.play()
     }
-   
+
 
 }
-var soundsOff = function(){
+var soundsOff = function () {
     var soundsButton = document.getElementById("soundsBtn")
-   if(areSoundsActive==true){
-        areSoundsActive = false 
-        soundsButton.innerHTML="Sounds an"
-   }else{
+    if (areSoundsActive == true) {
+        areSoundsActive = false
+        soundsButton.innerHTML = "Sounds an"
+    } else {
         areSoundsActive = true
-        soundsButton.innerHTML="Sounds aus"
-   } 
+        soundsButton.innerHTML = "Sounds aus"
+    }
+}
+
+var createObjects = function () {
+    //level one
+    vehicle[0] = new gameObject(50, 465, "pink car", 4, "left")
+    vehicle[1] = new gameObject(100, 465, "pink car", 4, "left")
+    vehicle[2] = new gameObject(150, 465, "pink car", 4, "left")
+
+    vehicle[3] = new gameObject(50, 432, "red car", 6, "right")
+    vehicle[4] = new gameObject(100, 432, "red car", 6, "right")
+
+    vehicle[5] = new gameObject(50, 396, "white LKW", 4, "left", 1.2)
+    vehicle[6] = new gameObject(100, 396, "white LKW", 4, "left", 1.2)
+
+    vehicle[7] = new gameObject(100, 360, "bagger", 4, "right")
+    vehicle[8] = new gameObject(140, 360, "bagger", 4, "right")
+    vehicle[9] = new gameObject(180, 360, "bagger", 4, "right")
+    vehicle[10] = new gameObject(220, 360, "bagger", 4, "right")
+
+    vehicle[11] = new gameObject(40, 325, "blue car", 4, "left")
+    vehicle[12] = new gameObject(80, 325, "blue car", 4, "left")
+
+
+    waterObj[0] = new gameObject(120, 250, "wood line", 2, "right", 2.5)
+    waterObj[1] = new gameObject(240, 250, "wood line", 2, "right", 2.5)
+
+    waterObj[2] = new gameObject(120, 217, "wood line", 3, "left", 2.5)
+    waterObj[3] = new gameObject(240, 217, "wood line", 3, "left", 2.5)
+
+
+    waterObj[4] = new gameObject(50, 182, "turtle1", 2, "left")
+    waterObj[5] = new gameObject(80, 182, "turtle1", 2, "left")
+    waterObj[6] = new gameObject(110, 182, "turtle1", 2, "left")
+
+    waterObj[7] = new gameObject(170, 182, "turtle1", 2, "left")
+    waterObj[8] = new gameObject(200, 182, "turtle1", 2, "left")
+    waterObj[9] = new gameObject(230, 182, "turtle1", 2, "left")
+
+    waterObj[10] = new gameObject(300, 182, "turtle1", 2, "left")
+    waterObj[11] = new gameObject(330, 182, "turtle1", 2, "left")
+    waterObj[12] = new gameObject(360, 182, "turtle1", 2, "left")
+
+    waterObj[13] = new gameObject(100, 148, "crocodile1", 1, "right", 2)
+    waterObj[14] = new gameObject(200, 148, "crocodile1", 1, "right", 2)
+
+
+    waterObj[15] = new gameObject(120, 112, "wood line", 1, "left", 2.5)
+    waterObj[16] = new gameObject(240, 112, "wood line", 1, "left", 2.5)
 }
